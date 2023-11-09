4ea475ae2a9b5e0506b05b42ba88b44eba9e8a7b
caocynthia <cynthiacao.work@gmail.com>
2023-11-08 19:19:04 -0500

    add color variables

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="navbar">
-      <Link to={`/`}>Home</Link>
+      <div className="d-flex gap-4">
+        <Link to={`/`}>Home</Link>
+        <Link to={`/`}>Search Clubs</Link>
+      </div>
       <Link to={`/profile`}>Profile</Link>
     </div>
   );
