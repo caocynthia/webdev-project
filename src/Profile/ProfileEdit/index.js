7dec2994ec9542655544da4736cd20eaab63d6f3
caocynthia <cynthiacao.work@gmail.com>
2023-11-09 22:45:43 -0500

    add basic components to profile page

---
+++ src/Profile/ProfileEdit/index.js
@@ -0,0 +1,17 @@
+import NavBar from "../Navbar";
+
+function ProfileEdit() {
+  return (
+    <>
+      <NavBar />
+      <div className="page-padding">
+        <i className="profile-pic bi bi-person-circle"></i>
+        <div className="d-flex gap-2 align-items-center">
+          <label>Name</label>
+          <input value="john doe" />
+        </div>
+      </div>
+    </>
+  );
+}
+export default ProfileEdit;
--- src/Profile/index.js
+++ src/Profile/index.js
@@ -6,9 +6,16 @@ function Profile() {
       <NavBar />
       <div className="page-padding">
         <i className="profile-pic bi bi-person-circle"></i>
-        <div className="d-flex gap-2 align-items-center">
-          <label>Name</label>
-          <input value="john doe" />
+        <div className="row">
+          <div className="col-2 d-flex flex-column gap-2">
+            <h5>Name</h5>
+            <p>bio text here lalala</p>
+          </div>
+          <div className="col-8 d-flex flex-column gap-2">
+            <h5>Likes</h5>
+            <div className="thing">item here</div>
+            <div className="thing">item here</div>
+          </div>
         </div>
       </div>
     </>
