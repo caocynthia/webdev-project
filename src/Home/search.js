4bb4768ef3e632e8c1aef15b0f4aa4cb2bc2e761
caocynthia <cynthiacao.work@gmail.com>
2023-11-18 22:45:45 -0500

    fix problem with results

--- src/Home/search.js
+++ src/Home/search.js
@@ -1,16 +1,13 @@
 import { fullTextSearch } from "../api/movie-service";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import Results from "../Results";
 
 function SearchMovie() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [results, setResults] = useState(null);
+  const [results, setResults] = useState();
 
   const search = async () => {
     const response = await fullTextSearch(searchTerm);
-    console.log(response.Search[1]);
-    setResults(response.Search);
+    setResults(response);
   };
 
   return (
@@ -23,11 +20,22 @@ function SearchMovie() {
         onChange={(e) => setSearchTerm(e.target.value)}
       />
 
+      {/* TODO: deal with case where nothing is searched but user presses search */}
       <button className="btn btn-primary" onClick={search}>
         Search
       </button>
 
-      <Results props={results} />
+      {results &&
+        results.Search.map((movie) => (
+          <div key={movie.imdbID} className="card">
+            {/* <Link to={`/project/album-details/${album.id}`}>
+              <img src={albumImageUrl(album)} />
+              <h2>{album.name}</h2>
+            </Link> */}
+            {console.log(movie.Title)}
+            <h1>{movie.Title}</h1>
+          </div>
+        ))}
     </div>
   );
 }
