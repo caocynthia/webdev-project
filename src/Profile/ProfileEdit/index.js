0708c32ffe0763b03e87292a07d44e9877cc9767
mathenachan <66972051+mathenachan@users.noreply.github.com>
2023-12-08 15:12:19 -0500

    profile edit

--- src/Profile/ProfileEdit/index.js
+++ src/Profile/ProfileEdit/index.js
@@ -1,13 +1,22 @@
+import { Link } from "react-router-dom";
 function ProfileEdit() {
   return (
     <>
       <div className="page-padding">
         <i className="profile-pic bi bi-person-circle"></i>
-        <div className="d-flex gap-2 align-items-center">
+        <div className="d-flex gap-2 align-items-center my-3">
           <label>Name</label>
-          <input value="john doe" />
+          <input className="form-control w-25" value="john doe" />
         </div>
-        <input type="email" value="john.doe@gmail.com"/>
+        <div className="d-flex gap-2 align-items-center my-3">
+        <label>Email</label>
+        <input className="form-control" type="email" value="john.doe@gmail.com"/>
+        </div>
+        <div className="d-flex gap-2 align-items-center my-3">
+        <label>Password</label>
+        <input className="form-control" type="password" value="doeremi"/>
+        </div>
+        <Link to="../Profile" className="btn btn-primary">Save Changes</Link>
       </div>
     </>
   );
