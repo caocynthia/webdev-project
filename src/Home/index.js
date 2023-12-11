447fcc9e7549ea11275dec486179dc346cb2f52b
caocynthia <cynthiacao.work@gmail.com>
2023-12-11 01:29:08 -0500

    add styling

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 function Home() {
   const [topMovies, setMovies] = useState([]);
-  const [recentMovies, setRecent] = useState([])
+  const [recentMovies, setRecent] = useState([]);
   const top = {
     method: "GET",
     headers: {
@@ -12,13 +12,14 @@ function Home() {
     },
   };
   const recent = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzE0NDQ1ZTJhMjFlMmRiYjUzYjY1NjQyNjE3NmY0NSIsInN1YiI6IjY1NzVlZGQ5N2EzYzUyMDE0ZTY5OWVlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._1pdIxA6xMlm-YnaNEII4yImoCc0e2UB77IBohSNapk'
-    }
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzE0NDQ1ZTJhMjFlMmRiYjUzYjY1NjQyNjE3NmY0NSIsInN1YiI6IjY1NzVlZGQ5N2EzYzUyMDE0ZTY5OWVlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._1pdIxA6xMlm-YnaNEII4yImoCc0e2UB77IBohSNapk",
+    },
   };
-  
+
   useEffect(() => {
     fetch(
       "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
@@ -30,55 +31,55 @@ function Home() {
       })
       .catch((err) => console.error(err));
 
-    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', recent)
-      .then(response => response.json())
-      .then(response => setRecent(response.results))
-      .catch(err => console.error(err));
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      recent
+    )
+      .then((response) => response.json())
+      .then((response) => setRecent(response.results))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
-    <div className="row">
-      <div className="col section-color">
-        <h1>Recent Movies</h1>
-        <div className="row">
-          {recentMovies.slice(0, 8).map((movie, index) => (
-            <div className="col-md-6 col-lg-3" key={index}>
+    <div className="d-flex flex-column gap-4">
+      <div className="mb-4">
+        <h1 className="mb-4">Top 5</h1>
+        <div className="d-flex gap-2">
+          {topMovies.slice(0, 5).map((movie, index) => (
+            <div className="card" key={index}>
               <Link className="link" to={`/MovieItem/${movie.id}`}>
-                <div className="card mb-4">
-                  <h5 className="searchMovieTitle">{movie.title}</h5>
-                  <p>Popularity: {movie.popularity}</p>
-                  <p>Vote Average: {movie.vote_average}</p>
-                  <img
-                    className="card-img-top"
-                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                </div>
+                <h5 className="searchMovieTitle">{movie.title}</h5>
+                <p>Popularity: {movie.popularity}</p>
+                <p>Vote Average: {movie.vote_average}</p>
+                <img
+                  className="card-img-top"
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt={movie.title}
+                />
               </Link>
             </div>
           ))}
         </div>
       </div>
-
-      <div className="col-2 section-color mx-5">
-        <div className="d-flex flex-column">
-          <h1>Top 7</h1>
-          <ul className="list-group">
-            {topMovies.slice(0, 7).map((movie, index) => (
-              <Link className="link" to={`/MovieItem/${movie.id}`} key={index}>
-                <li className="list-group-item my-1">
-                  <h6 className="searchMovieTitle">{movie.title}</h6>
-                  <div>Popularity: {movie.popularity}</div>
-                  <div>Vote Average: {movie.vote_average}</div>
-                  <img
-                    className="movieCards"
-                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                </li>
+      <div className="section-color col">
+        <h1 className="mb-4">Recent Movies</h1>
+        <div className="row g-0 gap-2">
+          {recentMovies.slice(0, 8).map((movie, index) => (
+            <div className="card" key={index}>
+              <Link className="link" to={`/MovieItem/${movie.id}`}>
+                <h5 className="searchMovieTitle">{movie.title}</h5>
+                <div>
+                  <p>Popularity: {movie.popularity}</p>
+                  <p>Vote Average: {movie.vote_average}</p>
+                </div>
+                <img
+                  className="card-img-top"
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt={movie.title}
+                />
               </Link>
-            ))}
-          </ul>
+            </div>
+          ))}
         </div>
       </div>
     </div>
